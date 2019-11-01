65b8458bb1244b02d50932208ce5729a8957ee40
Victor Alcantara <victoralcantara432@gmail.com>
2019-11-01 23:13:42 +0100

    using maps

---
+++ maps/letter-frequency.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	words := os.Args[1:]
+
+	frequency := frequencyOfFirstLetterIn(words)
+
+	report(frequency)
+}
+
+func frequencyOfFirstLetterIn(words []string) map[string]int {
+	frequency := make(map[string]int)
+	for _, word := range words {
+		first := strings.ToUpper(string(word[0]))
+		counter, found := frequency[first]
+		if found {
+			frequency[first] = counter + 1
+		} else {
+			frequency[first] = 1
+		}
+	}
+	return frequency
+}
+
+func report(frequency map[string]int) {
+	fmt.Println("Frequency of first letter in words:")
+
+	for first, counter := range frequency {
+		fmt.Printf("%s = %d\n", first, counter)
+	}
+}
